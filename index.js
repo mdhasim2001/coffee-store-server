829259bd895203f9fd5bfea095218b43ec07cd67
MD Hasim <mdhasim.nana.2001@gmail.com>
2025-01-23 23:01:42 +0600

    coffee data single data load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -32,6 +32,13 @@ async function run() {
     app.get("/coffees", async(req, res) => {
         const result = await coffeesColection.find().toArray()
         res.send(result)
+    })
+
+    app.get("/coffees/:id", async(req, res) =>{
+      const coffeesId = req.params.id;
+      const query = {_id: new ObjectId(coffeesId)}
+      const result = await coffeesColection.findOne(query);
+      res.send(result)
     })
 
     app.post("/coffees", async(req, res)=>{
